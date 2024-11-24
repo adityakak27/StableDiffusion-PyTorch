8948c42df80039d3ada172e3e04ccb7028ed8dbe
adityakak27 <146877885+adityakak27@users.noreply.github.com>
2024-11-24 18:44:23 +0530

    Update clip.py
    added a missing super init call

--- stable diffusion model/clip.py
+++ stable diffusion model/clip.py
@@ -55,10 +55,11 @@ class CLIPLayer(nn.Module):
 class CLIP(nn.Module):
 
     def __init__(self):
+        super().__init__()
         self.embedding = CLIPEmbedding(49408, 768, 77)
 
         self.layers = nn.Module([
-            CLIPLayer(12, 768) for i in range(12)
+            CLIPLayer(12, 768) for _ in range(12)
         ])
 
         self.layernorm = nn.LayerNorm(768)
